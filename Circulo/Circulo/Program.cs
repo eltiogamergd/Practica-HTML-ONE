176799d47bd52e990d3264393b5088da8f8d993b
Renzo Herrera Giuria <Renzo Herrera Giuria@DESKTOP-FPEFDKS>
2023-04-14 10:45:31 -0500

    Agregar archivos de proyecto.

---
+++ Circulo/Circulo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
+
+var radio = 1.1;
+var resultado = 0d;
+
+const double Pi = 3.14;
+
+Console.WriteLine("Bienvenido, calculemos círculos!");
+// Pi * radio*radio
+Console.WriteLine("Ingrese el valor del radio");
+
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Pi * radio * radio;
+Console.WriteLine("El área de tu círculo es " + resultado);
---
+++ Program.cs/Program.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola estudiante PLatzi");
+
+
+//Programa que calcula el área de un rectángulo.
+Double ladoA;
+Double ladoB;
+Double resultado;
+
+Console.WriteLine("Calcula el área de un rectángulo");
+Console.WriteLine("inserte el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("inserte el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El resultado es " + resultado);
